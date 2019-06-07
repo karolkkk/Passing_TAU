4b1c749f89bb469c1a829883605037e6c68213b5
karolkkk <s15195@pjwstk.edu.pl>
2019-06-07 13:31:49 +0200

    connection test

---
+++ src/test/java/pl/tau/db/dao/DbConnectTest.java
@@ -0,0 +1,42 @@
+package pl.tau.db.dao;
+
+/**
+ * Created by Owner on 07/06/2019.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class DbConnectTest {
+    private static final Logger LOGGER = Logger.getLogger(DbConnectTest.class.getCanonicalName());
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(1000);
+
+
+    @Test
+    public void createConnectionDbTest() {
+        DbConnect connection = new DbConnect();
+        assertNotNull(connection);
+    }
+
+    @Test
+    public void ConnectionTest() {
+        DbConnect connection = new DbConnect();
+        try {
+            assertNotNull(connection.getConnection());
+        }
+        catch(Exception e) {
+            LOGGER.log(Level.FINEST,"Failed connection");
+        }
+    }
+}
