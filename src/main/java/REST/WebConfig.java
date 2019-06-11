7e18cedcb0a0fb942f0f6c0013fc9fafd4d90912
karolkkk <s15195@pjwstk.edu.pl>
2019-06-11 04:41:04 +0200

    i'm done!

--- src/main/java/REST/WebConfig.java
+++ src/main/java/REST/WebConfig.java
@@ -11,8 +11,10 @@ public class WebConfig  {
 
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOrigins("*")
-                .allowedMethods("PUT", "DELETE", "GET", "POST");
+        registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
     }
-}
+    }
